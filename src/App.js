9f955e5c06616d9ef4772ead3bf445d300fa1910
OverFlowBIN <bin11788@gmail.com>
2022-02-02 14:05:11 +0900

    project start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import Customer from './components/Customer';
+
+const customer = {
+  'id': 1,
+  'image': 'https://placeimg.com//64/64/any',
+  'name': '김영빈',
+  'birthday': '961212',
+  'gender': '남자',
+  'job': '학생'
+}
+
 
 function App() {
   return (
-    <div className="gray-background">
-      <h2>let's go</h2>
-    </div>
+    <Customer 
+      id={customer.id}
+      image={customer.image}
+      name={customer.name}
+      birthday={customer.birthday}
+      gender={customer.gender}
+      job={customer.job}
+    />
   );
 }
 
---
+++ src/components/Customer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function Customer(props) {
+  return (
+    <>
+      <h2>{props.name}</h2>
+      <p>{props.birthday}</p>
+      <p>{props.gender}</p>
+      <p>{props.job}</p>
+    </>
+  )
+}
+
+function CustomerProfile (props) {
+  return (
+    
+  )
+}
+
+
+export default Customer;
