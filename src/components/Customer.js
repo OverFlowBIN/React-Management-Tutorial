c23eb903850ad77f465e9000d102be977094de6c
OverFlowBIN <bin11788@gmail.com>
2022-02-02 15:18:28 +0900

    customer_management_3

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,41 +1,21 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
 
 
 function Customer(props) {
   return (
-    <>
-      <CustomerProfile 
-        id={props.id}
-        image={props.image}
-        name={props.name}
-      />
-      <CustomerInfo 
-        birthday={props.birthday}
-        gender={props.gender}
-        job={props.job}
-      />
-    </>
+      <TableRow>
+        <TableCell>{props.id}</TableCell>
+        <TableCell>{<img src={props.image} alt='profile' />}</TableCell>
+        <TableCell>{props.name}</TableCell>
+        <TableCell>{props.birthday}</TableCell>
+        <TableCell>{props.gender}</TableCell>
+        <TableCell>{props.job}</TableCell>
+      </TableRow>
   )
 }
 
-function CustomerProfile (props) {
-  return (
-    <>
-      <img src={props.image} alt='profile'/>
-      <h2>{props.name}({props.id})</h2>
-    </>
-  )
-}
-
-
-function CustomerInfo(props) {
-  return (
-    <>
-      <p>{props.birthday}</p>
-      <p>{props.gender}</p>
-      <p>{props.job}</p>
-    </>
-  )
-}
 
 export default Customer;
